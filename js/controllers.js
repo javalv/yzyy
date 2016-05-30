ca62137c4f6afd0344fe60f5ec8d2463e6e29033
lin <java.com.lv@163.com>
2016-05-30 22:11:15 +0800

    技术点开发

--- js/controllers.js
+++ js/controllers.js
@@ -70,6 +70,28 @@ angular.module('starter.controllers', [])
 
         $scope.isActive = "1";
         initSel("1");
+
+        $scope.type = {
+            id:'1'
+        };
+        $scope.typeIds = [{
+            id:'1',
+            name:'药品'
+        },{
+            id:'2',
+            name:'器械'
+        }];
+        $scope.typeIdChanged = function () {
+            var typeId = $scope.type.id;
+            if(typeId == '1'){
+                $scope.typeId_1 = true;
+            }else{
+                $scope.typeId_1 = false;
+            }
+        }
+
+
+
     })
 
     .controller('multipleCtrl', function ($rootScope, $scope) {
